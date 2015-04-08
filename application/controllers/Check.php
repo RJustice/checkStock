634c7c754d9ebd1b57d428e4b8df8602b49b4800
lzh <1229757752@qq.com>
2015-04-08 15:18:43 +0800

    te

--- application/controllers/Check.php
+++ application/controllers/Check.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once(BASEPATH.'/application/libraries/phpQuery/phpQuery.php');
+require_once(APPPATH.'/libraries/phpQuery/phpQuery.php');
 
 
 class Check extends CI_Controller {
