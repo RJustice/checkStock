99120f3d996bc057c3394ec8657aff16ce72981a
lzh <1229757752@qq.com>
2015-04-18 15:20:18 +0800

    fix

--- application/controllers/Check.php
+++ application/controllers/Check.php
@@ -32,12 +32,17 @@ class Check extends CI_Controller {
         }elseif(preg_match('/creema/', $slink)){
             phpQuery::newDocumentFile($slink);
             $stock .= pq("#content-sub .detail-header_sidemenu")->html();
-        }elseif(preg_match('/rakuten/', $slink)){
-            
+        }elseif(preg_match('/rakuten/', $slink)){            
             phpQuery::$defaultCharset = 'euc-jp';
             phpQuery::newDocumentFile($slink);
             $stock .= pq("#rakutenLimitedId_cart")->html() . pq("#rakutenLimitedId_aroundCart")->html();
             // return $this->load->view('show_jp',['stock'=>$stock]);
+        }elseif(preg_match('/global\.rakuten/', $slink)){
+            phpQuery::newDocumentFile($slink);
+            $stock .= pq(".b-product-main .b-text")->html();
+        }elseif(preg_match('/knap/', $slink)){
+            phpQuery::newDocumentFile($slink);
+            $stock .= pq("#pagemain")->html();
         }
         return $this->load->view('show',['stock'=>$stock,'slink'=>$slink]);
     }
